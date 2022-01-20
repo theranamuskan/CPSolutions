df173fd90d50f0e43e37d04817930ba7dc4f55b2
Rana Muskan <72295127+theranamuskan@users.noreply.github.com>
2022-01-21 00:19:03 +0530

    Added Traversal of array solution

---
+++ traversal.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> vec= {34,22,55,66,7,2};
+    // vec.push_back(34);
+    // vec.push_back(79);
+    // vec.push_back(21);
+    // vec.push_back(3);
+    for (auto i : vec)
+    {
+        cout << i << " ";
+    }
+
+// for (int i = 0; i < vec.size(); i++)
+// {
+//     /* code */
+//     cout<<vec[i]<<" ";
+// }
+
+    return 0;
+}
