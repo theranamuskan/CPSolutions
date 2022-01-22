dc911764350a8b4b0a5577b560e38060e60b791b
Rana Muskan <72295127+theranamuskan@users.noreply.github.com>
2022-01-22 12:08:39 +0530

    Added Team Solution

---
+++ team.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    int p, v, t;
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> p >> v >> t;
+        if (p + v + t >= 2)
+        {
+            count++;
+        }
+    }
+    cout << count << endl;
+}
